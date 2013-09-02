a5eea75385cc085ef6e25614c51b8f233c662efb
James Nzomo <kazikubwa@gmail.com>
2013-09-02 10:53:23 +0300

    Algorithm correction

--- qargparser.cpp
+++ qargparser.cpp
@@ -1,14 +1,11 @@
 #include <QApplication>
 #include <QStringList>
 #include "qargparser.h"
- 
-QString QArgByKey(QString key, QChar sep)
+
+QString QArgByKey(QString key, QChar sep )
 {
-    if(qApp->arguments().size() < 2)
-        return QString::null;
-    bool sepd = sep != QChar('\0');
-    int pos = sepd ? qApp->arguments().indexOf(QRegExp('^'+ key + sep +"\\S*")) : qApp->arguments().indexOf(QRegExp(key));
-    return sepd?qApp->arguments().at(pos).split(sep).at(1)
-    :(++pos < qApp->arguments().size() ? qApp->arguments().at(pos) : QString::null);
+    bool sepd=sep!=QChar('\0');
+    int pos=sepd?qApp->arguments().indexOf(QRegExp('^'+key+sep+"\\S*")):qApp->arguments().indexOf(QRegExp(key));
+    return pos==-1?QString::null:
+    (sepd?qApp->arguments().at(pos).split(sep).at(1):(++pos<qApp->arguments().size()?qApp->arguments().at(pos):QString::null));
 }
- 
