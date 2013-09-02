bbb14324e31c20b2f6a508b687ea719a297c20da
James Nzomo <kazikubwa@gmail.com>
2013-09-02 08:47:00 +0300

    Update qargparser.cpp

--- qargparser.cpp
+++ qargparser.cpp
@@ -4,6 +4,8 @@
  
 QString QArgByKey(QString key, QChar sep)
 {
+    if(qApp->arguments().size() < 2)
+        return QString::null;
     bool sepd = sep != QChar('\0');
     int pos = sepd ? qApp->arguments().indexOf(QRegExp('^'+ key + sep +"\\S*")) : qApp->arguments().indexOf(QRegExp(key));
     return sepd?qApp->arguments().at(pos).split(sep).at(1)
